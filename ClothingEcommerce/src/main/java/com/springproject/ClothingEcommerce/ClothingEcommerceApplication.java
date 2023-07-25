ecaba1af2af5b27e1836d9e0f436243b2780c87d
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-25 20:01:04 +0545

    application setup

---
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/ClothingEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.ClothingEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingEcommerceApplication.class, args);
+	}
+
+}
