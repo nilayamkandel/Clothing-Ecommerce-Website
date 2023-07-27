81b65660f64e84a3b022f88e06f32b9166dbcbb8
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-27 12:33:28 +0545

    category database repo

--- ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/repository/CategoryRepository.java
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.springproject.ClothingEcommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.springproject.ClothingEcommerce.model.Category;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
 
 }
