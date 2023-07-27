219799457ed2856acb167c519c77f718e8d45fa5
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-27 13:55:57 +0545

    category controller

--- ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/controller/CategoryController.java
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/controller/CategoryController.java
@@ -2,10 +2,12 @@ package com.springproject.ClothingEcommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springproject.ClothingEcommerce.model.Category;
 import com.springproject.ClothingEcommerce.service.ICategoryService;
@@ -27,5 +29,30 @@ public class CategoryController {
 		catService.addCategory(category);
 		return "categoryadd";
 	}
+	
+	@GetMapping("/list")
+	public String getCategoryList(Model model) {
+		model.addAttribute("catlist",catService.getAllCategory());
+		return "categorylist";
+	}
+	
+	@GetMapping("/edit")
+	public String getCategoryEdit(@RequestParam int id,Model model) {
+		model.addAttribute("catedit",catService.getCategoryById(id));
+		return "categoryedit";
+	}
+	
+	@PostMapping("/update")
+	public String postCategoryEdit(@ModelAttribute Category category) {
+		catService.editCategory(category);
+		return "redirect:/category/list";
+	}
+	
+	@GetMapping("/delete")
+	public String getCategoryDelete(@RequestParam int id) {
+		catService.deleteCategory(id);
+		return "redirect:/category/list";
+	}
+	
 	
 }
