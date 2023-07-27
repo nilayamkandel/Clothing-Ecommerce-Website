fba223ead7344d0fae3bd8604d96d4453f1f30cb
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-27 12:33:42 +0545

    category service

--- ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/service/ICategoryService.java
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/service/ICategoryService.java
@@ -1,6 +1,12 @@
 package com.springproject.ClothingEcommerce.service;
 
+import com.springproject.ClothingEcommerce.model.Category;
+
 public interface ICategoryService {
 	
-	void addName();
+	void addCategory(Category category);
+	
+	void deleteCategory(int id);
+	
+	void editCategory(Category category);
 }
