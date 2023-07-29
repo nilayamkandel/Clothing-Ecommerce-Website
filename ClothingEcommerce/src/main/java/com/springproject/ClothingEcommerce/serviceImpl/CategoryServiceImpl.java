fedd35fc027e6206764fc8bed84abadb1e4faaf7
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-29 17:53:36 +0545

    edit category

--- ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/serviceImpl/CategoryServiceImpl.java
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/serviceImpl/CategoryServiceImpl.java
@@ -1,5 +1,7 @@
 package com.springproject.ClothingEcommerce.serviceImpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.springproject.ClothingEcommerce.model.Category;
@@ -28,6 +30,16 @@ public class CategoryServiceImpl implements ICategoryService{
 	public void editCategory(Category category) {
 		catRepo.save(category);
 		
+	}
+
+	@Override
+	public List<Category> getAllCategory() {
+		return catRepo.findAll();
+	}
+
+	@Override
+	public Category getCategoryById(int id) {		
+		return catRepo.findById(id).get();
 	}
 	
 	
