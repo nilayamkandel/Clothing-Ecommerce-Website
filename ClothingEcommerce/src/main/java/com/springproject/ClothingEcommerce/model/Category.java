40505c24a60a18c4b6ac4e883c76d4635f38783c
neelayam kandel <neelayamdottkandel@gmail.com>
2023-07-31 19:58:06 +0545

    category model

--- ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/model/Category.java
+++ ClothingEcommerce/src/main/java/com/springproject/ClothingEcommerce/model/Category.java
@@ -1,16 +1,11 @@
 package com.springproject.ClothingEcommerce.model;
 
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
